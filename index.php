98e0ea460d070b88e9e43b1a7dfce09dd00cb3ef
Ehohor <ehorvyalkin@gmail.com>
2026-06-03 23:36:30 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AnimePizza</title>
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+<header id="header">
+    <div class="contariner">
+        <div class="row d-flex align-items-center justify-content-between">
+            <div id="logo">
+                <a href="index.html">
+                    <img src="images/logo1.png" alt="Logo"/>
+                </a>
+            </div>
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#pizza">Pizza</a></li>
+                    <li><a href="#review">Review</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#contact-us">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<section id="home">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+<script src="libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="scripts/main.js"></script>
+</body>
+</html>
